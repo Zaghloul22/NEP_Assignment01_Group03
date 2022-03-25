d8ff4c65d421f2a2d507025803ff8173ce9cfa1f
Zaghloul22 <20FTT1410@student.pb.edu.bn>
2022-03-26 00:56:21 +0800

    finalize

--- src/cikupang/CiKuPangGame.java
+++ src/cikupang/CiKuPangGame.java
@@ -7,18 +7,21 @@ public class CiKuPangGame implements CiKuPangController{
     @Override
     public String welcomeMessage() {
 
-        return "           Welcome to          \n"+
-               "   		CI KU PANG Game    \n"+
-               "                               \n";
+        return "	Welcome to CI KU PANG Game\n"+
+        		"Choose between: Ci,KuPang\n\n"+
+        		 "For attacking side, if your hand matches the defensive one, you will HIT, otherwise MISS\n"+
+        		 "For defending, if your hand does not match the offensive one, you BLOCK the attack, otherwise you get HIT\n\n"+
+        		"Each player gets two HP, whoever runs out of HP first lose.\n\n"+
+        		"\r\n"+
+        		"ENJOY!";
     }
 
     @Override
     public String pleaseEnterOptionMessage(){
-        return  "         Please enter:         \n"+
-                "            1. CI            \n"+
-                "            2. KU           \n"+
-                "            3. PANG        \n"+
-                "                               \n";
+        return  "Please enter:\n"+
+                "1. CI\n"+
+                "2. KU\n"+
+                "3. PANG\n\n";
     }
 
     @Override
